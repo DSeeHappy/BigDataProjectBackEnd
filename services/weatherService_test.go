ef6cb7bd0c4fcec4d39b260d03dd40ba46f4a2eb
Daniel Sanchez <ds@dseehappy.com>
2023-11-27 18:41:10 -0700

    test

---
+++ services/weatherService_test.go
@@ -0,0 +1,23 @@
+package services
+
+import (
+	"Backend/models"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestValidateJob(t *testing.T) {
+	tests := []struct {
+		name   string
+		runner *models.Job
+		want   *models.ResponseError
+	}{}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			responseErr := validateJob(test.runner)
+			assert.Equal(t, test.want, responseErr)
+		})
+	}
+}
