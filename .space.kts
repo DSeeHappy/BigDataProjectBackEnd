6f566c82c33a82230e9e0e293380fc1df8d6189a
Daniel Sanchez <ds@dseehappy.com>
2023-12-07 02:31:04 +0000

    New file .space.kts

---
+++ .space.kts
@@ -0,0 +1,30 @@
+/**
+* JetBrains Space Automation
+* This Kotlin script file lets you automate build activities
+* For more info, see https://www.jetbrains.com/help/space/automation.html
+*/
+job("Build and push Docker") {
+    host("Build artifacts and a Docker image") {
+        // generate artifacts required for the image
+        shellScript {
+            content = """
+                ./generateArtifacts.sh
+            """
+        }
+
+        dockerBuildPush {
+            // Docker context, by default, project root
+            context = "docker"
+            // path to Dockerfile relative to project root
+            // if 'file' is not specified, Docker will look for it in 'context'/Dockerfile
+
+            val spaceRepo = "dseehappy.registry.jetbrains.space/p/prjkey/mydocker/myimage"
+            // image tags for 'docker push'
+            tags {
+                +"$spaceRepo:1.0.${"$"}JB_SPACE_EXECUTION_NUMBER"
+                +"$spaceRepo:latest"
+            }
+        }
+    }
+}
+
