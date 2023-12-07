b859c9cec123a8ae35856aebc95f80f00bd3df19
Daniel Sanchez <ds@dseehappy.com>
2023-12-07 14:40:45 -0700

    init rabbitmq logic

---
+++ server/subscriber.go
@@ -0,0 +1,49 @@
+package server
+
+import (
+	"Backend/utils"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"log"
+)
+
+func InitRabbitMQSubscriber() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	utils.FailOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	utils.FailOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	utils.FailOnError(err, "Failed to declare a queue")
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	utils.FailOnError(err, "Failed to register a consumer")
+
+	var forever chan struct{}
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+		}
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
+}
